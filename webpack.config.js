c3e919502ba98c0fbecc14ff00150b8a452b54c0
vynessa <vanessa.ejikeme@gmail.com>
2017-05-25 12:05:40 +0100

    [chore]: Webpack, react and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  entry: './js/app.js',
+  output: {
+    path: __dirname,
+    filename: 'js/bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
