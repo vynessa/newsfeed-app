5796a652128b15d1630db407dc1990e4baaf9fa7
vynessa <vanessa.ejikeme@gmail.com>
2017-05-29 14:45:59 +0100

    Add Category and Language Params to getSources function

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -1,5 +1,40 @@
 import axios from 'axios';
 
-const getSources = () => {
+const getSources = (category = '', lang = '', callback) => {
   const apiUrl = 'https://newsapi.org/v1/sources';
+
+  if (category === null || lang === null) {
+    axios.get(apiUrl)
+      .then((response) => {
+        callback(response.data.sources);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else if (lang.length > 0) {
+    const langUrl = `${apiUrl}?language=${lang}`;
+    axios.get(langUrl)
+      .then((response) => {
+        callback(response.data.sources);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    const catUrl = `${apiUrl}?category=${category}`;
+    axios.get(catUrl)
+      .then((response) => {
+        callback(response.data.sources);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 };
+
+const getArticles = (source, apiKey, sortBy) => {
+  apiKey = 'b3af0273f37245359b9131db78464ad4';
+
+  axios.get();
+};
+
