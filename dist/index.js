df36d0407114e0865ba710fd4f0045d7c8d0445c
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-06-22 03:41:39 +0100

    Remove dist folder from .gitignore

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+const Server = require('../server.js'),
+  webpack = require('webpack'),
+  port = (process.env.PORT || 8080),
+  app = Server.app();
+
+let config;
+if (process.env.NODE_ENV === 'development') {
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  config = require('../webpack.config.babel.js');
+
+  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPathdist
+  }));
+} else {
+  config = require('../webpack.deployment.config.js');
+}
+
+const compiler = webpack(config);
+
+
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
