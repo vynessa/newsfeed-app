130ec5f2f44053084a421ced528926422e3aab2d
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-07-02 23:27:10 +0100

    Add test suite

--- __tests__/state-functions.tests.jsx
+++ __tests__/state-functions.tests.jsx
@@ -4,4 +4,8 @@ describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
   });
+
+  it('knows that 2 and 5 make 7', () => {
+    expect(2 + 5).toBe(7);
+  });
 });
