9acb6c033fc69fca6792aa28919055ba8fee6f2f
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-07-11 18:17:22 +0100

    [chore]: Create Image component for modularity on Home component * Import images into Image.jsx * Create an object with image names as keys and slider information as values * Import Image component into Home as pass in the values as props

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,49 +1,44 @@
 import React from 'react';
 import { Slider, Slide } from 'react-materialize';
-import business from '../../../public/img/business-man-1031755_1920-1080x675.jpg';
-import whatsNew from '../../../public/img/whats-new.jpg';
-import theGoodNews from '../../../public/img/the-good-news.jpg';
-import newsWallpaper from '../../../public/img/newspaper-wallpaper.jpg';
+import images from './Images.jsx';
 
 /**
- * @class Home
- * @extends {React.Component}
+ * @description Home component which contains slider and slide images for the landing page
+ * @function Home
  */
-class Home extends React.Component {
+const Home = () => {
   /**
    * @description Returns JSX Home page Component
    * @returns {JSX.Element} Home
    * @memberof Home
    */
-  render() {
-    return (
-      <Slider className="slider fullscreen">
-        <Slide
-          src={business}
-          title="Welcome to e-Feeds">
-          View Latest feeds from different Sources
-        </Slide>
-        <Slide
-          src={whatsNew}
-          title="Check out what's new on the news today"
-          placement="left">
-          Login to access the Latest gist
-        </Slide>
-        <Slide
-          src={theGoodNews}
-          title="Get the latest news on the fly"
-          placement="right">
-          Both good and bad
-        </Slide>
-        <Slide
-          src={newsWallpaper}
-          title="Explore different news categories..."
-          placement="right">
-          From Business news to Sports, Fashion, Music...
-        </Slide>
-      </Slider>
-    );
-  }
-}
+  return (
+    <Slider className="slider fullscreen">
+      <Slide
+        src={images.business.image}
+        title={images.business.title}>
+        {images.business.text}
+      </Slide>
+      <Slide
+        src={images.whatsNew.image}
+        title={images.whatsNew.title}
+        placement="left">
+        {images.whatsNew.text}
+      </Slide>
+      <Slide
+        src={images.theGoodNews.image}
+        title={images.theGoodNews.title}
+        placement="right">
+        {images.theGoodNews.text}
+      </Slide>
+      <Slide
+        src={images.newsWallpaper.image}
+        title={images.newsWallpaper.title}
+        placement="right">
+        {images.newsWallpaper.text}
+      </Slide>
+    </Slider>
+  );
+};
 
 export default Home;
---
+++ src/js/components/Images.jsx
@@ -0,0 +1,30 @@
+import business from '../../../public/img/business-man-1031755_1920-1080x675.jpg';
+import whatsNew from '../../../public/img/whats-new.jpg';
+import theGoodNews from '../../../public/img/the-good-news.jpg';
+import newsWallpaper from '../../../public/img/newspaper-wallpaper.jpg';
+
+const Images = {
+  business: {
+    image: business,
+    title: 'Welcome to e-Feeds',
+    text: 'View Latest feeds from different Sources'
+  },
+
+  whatsNew: {
+    image: whatsNew,
+    title: 'Check out what\'s new on the news today',
+    text: 'Login to access the Latest gist'
+  },
+  theGoodNews: {
+    image: theGoodNews,
+    title: 'Get the latest news on the fly',
+    text: 'Both good and bad'
+  },
+  newsWallpaper: {
+    image: newsWallpaper,
+    title: 'Explore different news categories...',
+    text: 'From Business news to Sports, Fashion, Music...'
+  }
+};
+
+export default Images;
