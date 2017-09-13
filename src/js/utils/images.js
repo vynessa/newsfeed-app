27c5497ab5578e11cef1bb4f2e276241e77052fd
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-09-13 12:40:15 +0100

    Re-add the old images and information

--- src/js/utils/images.js
+++ src/js/utils/images.js
@@ -8,6 +8,21 @@ const Images = [
     image: business,
     title: 'Welcome to e-Feeds',
     text: 'View Latest feeds from different Sources'
+  },
+  {
+    image: whatsNew,
+    title: 'Check out what\'s new on the news today',
+    text: 'Login to access the Latest gist'
+  },
+  {
+    image: theGoodNews,
+    title: 'Get the latest news on the fly',
+    text: 'View Latest feeds from different Sources'
+  },
+  {
+    image: newsWallpaper,
+    title: 'Explore different news categories',
+    text: 'From Business news to Sports, Fashion, Music...'
   }
 ];
 
